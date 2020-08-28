9fc3d8127127c66f0be33c1dfe7869af42a70c2e
Laercio Ariel Macedo <laerciombr@gmail.com>
2020-08-28 01:12:35 -0300

    Arquivo de banco mongodb

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "manager",
+        pwd: "administrator",
+        roles: [
+            {
+                role: "readWrite",
+                db: "cinema"
+            }
+        ]
+    }
+);
